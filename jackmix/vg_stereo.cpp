b822c2f8e61b69a49f9bce8a3c9d028d0cd251df
Arnold Krille <arnold@arnoldarts.de>
2004-03-09 15:11:17 +0000

    Make us of the tickmarks. and some layout fixes.
    still needs a lot of work...

--- jackmix/vg_stereo.cpp
+++ jackmix/vg_stereo.cpp
@@ -4,6 +4,7 @@
 
 #include "jack_backend.h"
 #include "volumeslider.h"
+#include "qtickmarks.h"
 
 #include <iostream>
 #include <qlabel.h>
@@ -49,20 +50,21 @@ VGStereoChannelWidget::VGStereoChannelWidget( QString in, VolumeGroup* group, QW
 	setFrameStyle( QFrame::Panel|QFrame::Raised );
 	QVBoxLayout* _layout = new QVBoxLayout( this );
 	_layout->setMargin( 2 );
-	_layout->addWidget( new QLabel( _in, this ) );
+	_layout->addWidget( new QLabel( _group->name(), this ) );
 	QHBoxLayout* _layout2 = new QHBoxLayout( _layout );
 	VolumeSlider* tmp;
-	tmp = new VolumeSlider( _group->name() + "_L", 1, Qt::Vertical, this );
+	tmp = new VolumeSlider( "L", 1, Qt::Vertical, this );
 	connect( tmp, SIGNAL( valueChanged( QString,float ) ), this, SLOT( valueChanged( QString,float ) ) );
 	_layout2->addWidget( tmp );
-	tmp = new VolumeSlider( _group->name() + "_R", 1, Qt::Vertical, this );
+	_layout2->addWidget( new QTickmarks( -36, 12, BottomToTop, posLeft|posRight, this ) );
+	tmp = new VolumeSlider( "R", 1, Qt::Vertical, this );
 	connect( tmp, SIGNAL( valueChanged( QString,float ) ), this, SLOT( valueChanged( QString,float ) ) );
 	_layout2->addWidget( tmp );
 }
 VGStereoChannelWidget::~VGStereoChannelWidget() {
 }
 void VGStereoChannelWidget::valueChanged( QString channel, float value ) {
-std::cerr << "VGStereoChannelWidget::valueChanged( " << channel << ", " << value << " )" << std::endl;
-	BACKEND->setVolume( _in, channel, value );
+	//std::cerr << "VGStereoChannelWidget::valueChanged( " << channel << ", " << value << " )" << std::endl;
+	BACKEND->setVolume( _in, _group->name() + "_" + channel, value );
 }
 
