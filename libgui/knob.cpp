fa84052483c807b28b4fe305608c09e6792d5bdf
Arnold Krille <arnold@arnoldarts.de>
2007-03-26 20:11:04 +0000

    Fix copyrights.

--- libgui/knob.cpp
+++ libgui/knob.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright ( C ) 2006 Arnold Krille <arnold@arnoldarts.de>
+    Copyright 2007 Arnold Krille <arnold@arnoldarts.de>
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
