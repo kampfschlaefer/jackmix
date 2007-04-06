c7946f79398e73efb735298650dcfc48887598e4
Arnold Krille <arnold@arnoldarts.de>
2007-04-06 16:01:42 +0000

    First real version.
    Have to think about these configs and saving and restore some more...

--- libqlash/qlash.cpp
+++ libqlash/qlash.cpp
@@ -1,3 +1,96 @@
 
 #include "qlash.h"
 
+#include <QtCore/QDebug>
+
+namespace qLash {
+
+qLashClient::qLashClient( QString clientname, int argc, char** argv, QObject* p ) : QObject( p ) {
+	_client = lash_init( lash_extract_args( &argc, &argv ), clientname.toStdString().c_str(), LASH_Config_Data_Set | LASH_Config_File, LASH_PROTOCOL( 2,0 ) );
+
+	if ( isConnected() ) {
+
+		lash_event_t* event = lash_event_new_with_type( LASH_Client_Name );
+		lash_event_set_string( event, clientname.toStdString().c_str() );
+		lash_send_event( _client, event );
+
+	}
+
+	startTimer( 250 ); // Fire four times a second.
+}
+qLashClient::~qLashClient() {
+}
+
+bool qLashClient::isConnected() {
+	return lash_enabled( _client );
+}
+
+void qLashClient::saveToDirFinished() {
+	lash_send_event( _client, lash_event_new_with_type( LASH_Save_File ) );
+}
+void qLashClient::saveToConfigFinished() {
+	lash_send_event( _client, lash_event_new_with_type( LASH_Save_Data_Set ) );
+}
+void qLashClient::restoreFromDirFinished() {
+	lash_send_event( _client, lash_event_new_with_type( LASH_Restore_File ) );
+}
+void qLashClient::restoreFromConfigFinished() {
+	lash_send_event( _client, lash_event_new_with_type( LASH_Restore_Data_Set ) );
+}
+
+void qLashClient::saveToConfig( int data ) {
+	qDebug() << "qLashClient::saveToConfig(" << data << ")";
+	qDebug() << " sender()" << sender();
+	if ( sender() && sender()->objectName().isEmpty() ) {
+		lash_config_t* config = lash_config_new_with_key( sender()->objectName().toStdString().c_str() );
+		lash_config_set_value_int( config, data );
+		lash_send_config( _client, config );
+		saveToConfigFinished();
+	}
+}
+
+void qLashClient::timerEvent( QTimerEvent* ) {
+	while ( isConnected() && lash_get_pending_event_count( _client ) > 0 ) {
+		lash_event_t* event = lash_get_event( _client );
+		//qDebug() << "Have event of type" << lash_event_get_type( event );
+
+		switch ( lash_event_get_type( event ) ) {
+			case LASH_Client_Name:
+				qDebug() << "Event: This clients name is:" << lash_event_get_string( event );
+				break;
+			/*case LASH_Jack_Client_Name:
+				qDebug() << "Event: This clients JACK-name is:" << lash_event_get_string( event );
+				break;*/
+			/*case LASH_Alsa_Client_ID:
+				qDebug() << "Event: This clients Alsa-ID is:" << lash_event_get_string( event );
+				break;*/
+			case LASH_Save_File:
+				qDebug() << "Event: Should save data into dir" << lash_event_get_string( event );
+				emit saveToDir( lash_event_get_string( event ) );
+				break;
+			case LASH_Restore_File:
+				qDebug() << "Event: Should restore data from dir" << lash_event_get_string( event );
+				emit restoreFromDir( lash_event_get_string( event ) );
+				break;
+			case LASH_Save_Data_Set:
+				qDebug() << "Event: Should save data in configs";
+				emit saveToConfig();
+				break;
+			case LASH_Restore_Data_Set:
+				qDebug() << "Event: Should restore data from configs";
+				emit restoreFromConfig();
+				break;
+			case LASH_Quit:
+				qDebug() << "Event: Should terminate now";
+				emit quitApp();
+				break;
+			default:
+				qDebug() << "Handling of event" << lash_event_get_type( event ) << "isn't implemented yet!";
+		}
+
+		lash_event_destroy( event );
+	}
+}
+
+}; // qLash
+
--- libqlash/qlash.h
+++ libqlash/qlash.h
@@ -3,14 +3,94 @@
 
 #include <QtCore/QObject>
 
+#include <lash/lash.h>
+
 namespace qLash {
 
 class qLashClient : public QObject
 {
 	Q_OBJECT
 	public:
-		qLashClient( QObject* =0 );
+		qLashClient( QString clientname, int argc =0, char** argv =0, QObject* =0 );
 		virtual ~qLashClient();
+
+		bool isConnected();
+
+		/**
+		 * Only for DEBUGGING!
+		 *
+		 * @todo Remove me!
+		 */
+		lash_client_t* client() { return _client; }
+
+	public slots:
+		/**
+		 * @brief Saving to dir finished
+		 *
+		 * Tell the server that saving to directory is finished.
+		 */
+		void saveToDirFinished();
+		/**
+		 * @brief Saving to config finished
+		 *
+		 * Tell the server that saving to configs is finished.
+		 */
+		void saveToConfigFinished();
+		/**
+		 * @brief Restoring from dir finished
+		 *
+		 * Tell the server that restoring from directory is finished.
+		 */
+		void restoreFromDirFinished();
+		/**
+		 * @brief Restoring from config finished
+		 *
+		 * Tell the server that restoring from configs is finished.
+		 */
+		void restoreFromConfigFinished();
+
+		/**
+		 * @todo not finished yet. Have to think about saving/restoring to/from configs a while.
+		 */
+		void saveToConfig( int );
+
+	signals:
+		/**
+		 * @brief The server tells us to quit immediatly.
+		 *
+		 * ...without saving.
+		 */
+		void quitApp();
+		/**
+		 * @brief Save data into directory
+		 *
+		 * You should ( have to ) call saveToDirFinished() afterwards.
+		 */
+		void saveToDir( QString dir );
+		/**
+		 * @brief Save data into configs
+		 *
+		 * You should ( have to ) call saveToConfigFinished() afterwards.
+		 */
+		void saveToConfig();
+		/**
+		 * @brief Restore data from directory
+		 *
+		 * You should ( have to ) call restoreFromDirFinished() afterwards.
+		 */
+		void restoreFromDir( QString dir );
+		/**
+		 * @brief Restore data from configs
+		 *
+		 * You should ( have to ) call restoreFromConfigFinished() afterwards.
+		 */
+		void restoreFromConfig();
+
+	protected:
+		void timerEvent( QTimerEvent* );
+
+	private:
+		lash_client_t *_client;
 
 }; // qLashClient
 
